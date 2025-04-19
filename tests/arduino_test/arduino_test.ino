e05d0192669fcccf84cf3d7674e86e12d37ae673
igorkll <frerther@gmail.com>
2025-04-19 05:29:03 +0300

    backup

--- tests/arduino_test/arduino_test.ino
+++ tests/arduino_test/arduino_test.ino
@@ -4,13 +4,19 @@ extern "C" {
   #include <lualib.h>
 }
 
+static int _uartout(lua_State* lua) {
+  Serial.println(luaL_checkstring(lua, 0));
+  return 0;
+} 
+
 void setup() {
   Serial.begin(9600);
 
   Serial.println(F("try start lua"));
   lua_State* lua = luaL_newstate();
-  //luaL_openlibs(lua);
-  if (luaL_dostring(lua, "print('test')") != LUA_OK) {
+  lua_pushcfunction(lua, _uartout);
+  lua_setglobal(lua, "uartout");
+  if (luaL_dostring(lua, "uartout('hello, avr-lua!')") != LUA_OK) {
     const char* err = lua_tostring(lua, -1);
     Serial.print(F("lua crashed: "));
     Serial.println(err);
