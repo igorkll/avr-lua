89b64e6dbf3367d7026f6566a7a0d4c2c4603ade
igorkll <frerther@gmail.com>
2025-04-19 05:12:03 +0300

    Update arduino_test.ino

--- tests/arduino_test/arduino_test.ino
+++ tests/arduino_test/arduino_test.ino
@@ -6,12 +6,15 @@ extern "C" {
 
 void setup() {
   Serial.begin(9600);
-  
+
+  printf("try start lua");
   lua_State* lua = luaL_newstate();
   luaL_openlibs(lua);
   if (luaL_dostring(lua, "print('test')") != LUA_OK) {
-      const char* err = lua_tostring(lua, -1);
-      printf("lua crashed: %s", err);
+    const char* err = lua_tostring(lua, -1);
+    printf("lua crashed: %s", err);
+  } else {
+    printf("lua end");
   }
   lua_close(lua);
 }
