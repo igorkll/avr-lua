d37b0f82b223f9f08a6b9b4eb1a31f43a83046c5
igorkll <frerther@gmail.com>
2025-04-19 04:31:59 +0300

    try port lua to avr

---
+++ README.md
@@ -0,0 +1,3 @@
+# arv-lua
+
+it implements lua version 5.3.6 on AVR microcontrollers
---
+++ tests/arduino_test/arduino_test.ino
@@ -0,0 +1,21 @@
+extern "C" {
+  #include <lua.h>
+  #include <lauxlib.h>
+  #include <lualib.h>
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  lua_State* lua = luaL_newstate();
+  luaL_openlibs(lua);
+  if (luaL_dostring(lua, "print('test')") != LUA_OK) {
+      const char* err = lua_tostring(lua, -1);
+      printf("lua crashed: %s", err);
+  }
+  lua_close(lua);
+}
+
+void loop() {
+  
+}
