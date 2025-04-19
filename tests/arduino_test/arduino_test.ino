24a813defc912f611fb2065cf061c50a399fc99c
igorkll <frerther@gmail.com>
2025-04-19 05:33:47 +0300

    Update arduino_test.ino

--- tests/arduino_test/arduino_test.ino
+++ tests/arduino_test/arduino_test.ino
@@ -12,18 +12,22 @@ static int _uartout(lua_State* lua) {
 void setup() {
   Serial.begin(9600);
 
-  Serial.println(F("try start lua"));
   lua_State* lua = luaL_newstate();
   lua_pushcfunction(lua, _uartout);
   lua_setglobal(lua, "uartout");
-  if (luaL_dostring(lua, "uartout('hello, avr-lua!')") != LUA_OK) {
-    const char* err = lua_tostring(lua, -1);
-    Serial.print(F("lua crashed: "));
-    Serial.println(err);
-  } else {
-    Serial.println(F("lua end"));
+
+  while (true) {
+    String code = Serial.readString();
+    
+    Serial.println(F("lua start"));
+    if (luaL_dostring(lua, code.c_str()) != LUA_OK) {
+      const char* err = lua_tostring(lua, -1);
+      Serial.print(F("lua crashed: "));
+      Serial.println(err);
+    } else {
+      Serial.println(F("lua end"));
+    }
   }
-  lua_close(lua);
 }
 
 void loop() {
